aa03b5ccec96fd0a37a8217db58359061eb62584
Utku Doğrusöz <utku.dogrusoz@gmail.com>
2021-06-11 20:17:50 +0300

    category query  commit

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,17 +1,32 @@
 const Movie = require("../models/Movie");
 
-const getAllMovies = async (req, res) => {
+const getMovies = async (req, res) => {
 
     try {
-        const movies = await Movie
-            .find()
-            .populate("movieCategories movieDirector movieCasts")
-            .select('movieName movieCategories movieCasts movieDirector movieDescription movieImage movieYear slug');
+        console.log(req.body._id == null);
 
-        res.status(200).json({
-            status: "success",
-            data: movies
-        });
+        if (req.body._id == null) {
+            const movies = await Movie
+                .find()
+                .populate("movieCategories movieDirector movieCasts")
+                .select('movieName movieCategories movieCasts movieDirector movieDescription movieImage movieYear slug');
+            res.status(200).json({
+                status: "success",
+                data: movies
+            });
+        }
+
+        else {
+            const movies = await Movie
+                .find({ movieCategories: req.body._id })
+                .populate("movieCategories movieDirector movieCasts")
+                .select('movieName movieCategories movieCasts movieDirector movieDescription movieImage movieYear slug');
+            res.status(200).json({
+                status: "success",
+                data: movies
+            });
+        }
+       
     } catch (error) {
         res.status(400).json({
             status: "fail",
@@ -22,4 +37,4 @@ const getAllMovies = async (req, res) => {
 
 }
 
-module.exports = { getAllMovies }
+module.exports = { getMovies }
--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const movieController = require("../controllers/movieController");
 
-router.route("/getAllMovies").get(movieController.getAllMovies);
+router.route("/getallmovies").post(movieController.getMovies);
 
 module.exports = router;
